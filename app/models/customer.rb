504fee5c5744e2a2242f5f5eafe469998ac052d4
John Emmanuel Mison <misonjohnemmanuel@gmail.com>
2025-10-06 20:11:29 +0800

    add customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+    validates :first_name, :last_name, :phone_number, presence: true
+    validates :phone_number, format: { with: /\A\d+\z/, message: "only allows numbers" }
 end
