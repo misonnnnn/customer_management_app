1ed460668543752934ace1a11641aff50767947e
John Emmanuel Mison <misonjohnemmanuel@gmail.com>
2025-10-06 22:30:15 +0800

    add birthday date validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,9 +5,16 @@ class Customer < ApplicationRecord
     validates :first_name, :last_name, length: { maximum: 50 }
     validates :phone_number, format: { with: /\A\d+\z/, message: "only allows numbers" }
     validates :phone_number, uniqueness: true
+    validate :date_of_birth_cannot_be_in_the_future
 
     def full_name
         "#{first_name} #{last_name}"
+    end
+
+    def date_of_birth_cannot_be_in_the_future
+        if date_of_birth.present? && date_of_birth > Date.today
+            errors.add(:date_of_birth, "can't be in the future")
+        end
     end
 
     scope :active, -> { where(status: "active") }
